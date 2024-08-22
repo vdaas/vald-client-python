9b1de6c9c2aa0266ec4c65d585352ce3ba02a8c9
Vdaas CI <vald@vdaas.org>
2024-08-22 05:54:57 +0000

    Update [vald sha: 4560a2bbbd406bb63a2187e16c69ebebf5e3eec3]
    Signed-off-by: Vdaas CI <vald@vdaas.org>

--- src/vald/v1/vald/index_pb2.py
+++ src/vald/v1/vald/index_pb2.py
@@ -26,7 +26,7 @@ from google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
 from vald.v1.payload import payload_pb2 as vald_dot_v1_dot_payload_dot_payload__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x18vald/v1/vald/index.proto\x12\x07vald.v1\x1a\x1cgoogle/api/annotations.proto\x1a\x1dvald/v1/payload/payload.proto2\x8d\x03\n\x05Index\x12Q\n\tIndexInfo\x12\x11.payload.v1.Empty\x1a\x1c.payload.v1.Info.Index.Count\"\x13\x82\xd3\xe4\x93\x02\r\x12\x0b/index/info\x12V\n\x0bIndexDetail\x12\x11.payload.v1.Empty\x1a\x1d.payload.v1.Info.Index.Detail\"\x15\x82\xd3\xe4\x93\x02\x0f\x12\r/index/detail\x12\x62\n\x0fIndexStatistics\x12\x11.payload.v1.Empty\x1a!.payload.v1.Info.Index.Statistics\"\x19\x82\xd3\xe4\x93\x02\x13\x12\x11/index/statistics\x12u\n\x15IndexStatisticsDetail\x12\x11.payload.v1.Empty\x1a\'.payload.v1.Info.Index.StatisticsDetail\" \x82\xd3\xe4\x93\x02\x1a\x12\x18/index/statistics/detailBR\n\x1aorg.vdaas.vald.api.v1.valdB\tValdIndexP\x01Z\'github.com/vdaas/vald/apis/grpc/v1/valdb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x18vald/v1/vald/index.proto\x12\x07vald.v1\x1a\x1cgoogle/api/annotations.proto\x1a\x1dvald/v1/payload/payload.proto2\xf1\x03\n\x05Index\x12Q\n\tIndexInfo\x12\x11.payload.v1.Empty\x1a\x1c.payload.v1.Info.Index.Count\"\x13\x82\xd3\xe4\x93\x02\r\x12\x0b/index/info\x12V\n\x0bIndexDetail\x12\x11.payload.v1.Empty\x1a\x1d.payload.v1.Info.Index.Detail\"\x15\x82\xd3\xe4\x93\x02\x0f\x12\r/index/detail\x12\x62\n\x0fIndexStatistics\x12\x11.payload.v1.Empty\x1a!.payload.v1.Info.Index.Statistics\"\x19\x82\xd3\xe4\x93\x02\x13\x12\x11/index/statistics\x12u\n\x15IndexStatisticsDetail\x12\x11.payload.v1.Empty\x1a\'.payload.v1.Info.Index.StatisticsDetail\" \x82\xd3\xe4\x93\x02\x1a\x12\x18/index/statistics/detail\x12\x62\n\rIndexProperty\x12\x11.payload.v1.Empty\x1a%.payload.v1.Info.Index.PropertyDetail\"\x17\x82\xd3\xe4\x93\x02\x11\x12\x0f/index/propertyBR\n\x1aorg.vdaas.vald.api.v1.valdB\tValdIndexP\x01Z\'github.com/vdaas/vald/apis/grpc/v1/valdb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -42,6 +42,8 @@ if not _descriptor._USE_C_DESCRIPTORS:
   _globals['_INDEX'].methods_by_name['IndexStatistics']._serialized_options = b'\202\323\344\223\002\023\022\021/index/statistics'
   _globals['_INDEX'].methods_by_name['IndexStatisticsDetail']._loaded_options = None
   _globals['_INDEX'].methods_by_name['IndexStatisticsDetail']._serialized_options = b'\202\323\344\223\002\032\022\030/index/statistics/detail'
+  _globals['_INDEX'].methods_by_name['IndexProperty']._loaded_options = None
+  _globals['_INDEX'].methods_by_name['IndexProperty']._serialized_options = b'\202\323\344\223\002\021\022\017/index/property'
   _globals['_INDEX']._serialized_start=99
-  _globals['_INDEX']._serialized_end=496
+  _globals['_INDEX']._serialized_end=596
 # @@protoc_insertion_point(module_scope)
